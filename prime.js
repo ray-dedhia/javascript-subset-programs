7f3c89649a727e43903c68fe0723f30789fb3698
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2021-12-15 07:11:39 -0500

    Update

--- prime.js
+++ prime.js
@@ -1,32 +1,36 @@
-function is_prime(num, past_primes = null) {
+function is_prime1(num, past_primes) {
     /****************************************************
-    * Return whether or not num is prime
-    *
-    * Parameter past_primes is list of all prime numbers
-    * less than n (optional)
+    * Given list of all prime numbers less than num,
+    * return whether or not num is prime
     ****************************************************/
-    // if past_primes list is not null
-    if (past_primes != null) {
-        var j;
-        j = 0;
-        // check if num is divisible by past primes
-        while (j < past_primes.length) {
-            // if it is, return false
-            if (num % past_primes[j] == 0) {
-                return false;
-            }
-            j = j + 1;
+    var j;
+    j = 0;
+    var max_check;
+    max_check = Math.floor(Math.sqrt(num) + 1);
+    // check if num is divisible by any primes less than Math.floor(Math.sqrt(num) + 1)
+    while (j < past_primes.length) {
+        // if it is, return false
+        if ((past_primes[j] < max_check) && (num % past_primes[j] == 0)) {
+            return false;
         }
-        // else return true
-        return true;
+        j = j + 1;
     }
+    // else return true
+    return true;
+}
 
-    // if past primes is null
+function is_prime2(num) {
+    /****************************************************
+    * Return whether or not num is prime (brute force
+    * up to Math.floor(Math.sqrt(num) + 1)
+    ****************************************************/
     var check_div;
     check_div = 2;
+    var max_check;
+    max_check = Math.floor(Math.sqrt(num) + 1);
     // check if num is divisible by all numbers check_div
     // in range 2 <= check_div < num
-    while (check_div < num) {
+    while (check_div < max_check) {
         // if it is, return false
         if (num % check_div == 0) {
             return false;
@@ -49,7 +53,7 @@ function get_nth_prime(n) {
     prime_count = 0;
     while (prime_count < n) {
         var num_is_prime;
-        num_is_prime = is_prime(num, past_primes);
+        num_is_prime = is_prime1(num, past_primes);
         // if prime, add to past_primes and increment prime_count
         if (num_is_prime) {
             past_primes.push(num);
@@ -68,10 +72,10 @@ function test() {
     console.log(n + "th prime = " + get_nth_prime(n));
     var a;
     a = 351;
-    console.log(a + " is prime = " + is_prime(a));
+    console.log(a + " is prime = " + is_prime2(a));
     var b;
     b = 1483;
-    console.log(b + " is prime = " + is_prime(b));
+    console.log(b + " is prime = " + is_prime2(b));
 }
 
 test();
