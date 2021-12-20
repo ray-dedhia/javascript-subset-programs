6f565b8d114fc3424ae8d4a15532a0094109c8bf
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2021-12-19 19:27:32 -0500

    Updated braille.js

--- braille.js
+++ braille.js
@@ -1,22 +1,21 @@
-function parse(unicode) {
+function parse_unicode(unicode) {
     /****************************************************
     * Convert unicode hexadecimal value to unicode
     ****************************************************/
     return JSON.parse('\"\\u' + unicode + '\"');
 }
 
-function gen_unicode_maps() {
+function gen_maps() {
     /****************************************************
-    * Return [object mapping unicode braille to ascii glyphs,
-    * object mapping ascii glyphs to unicode braille] 
+    * Return [object mapping braille to letters,
+    * object mapping letters to braille]
     *
     * Uses https://en.wikipedia.org/wiki/Braille_ASCII
     ****************************************************/
-    // Map braille unicode to ascii glyphs and vice versa
-    var unicode_to_glyph;
-    unicode_to_glyph = {};
-    var glyph_to_unicode;
-    glyph_to_unicode = {};
+    var braille_to_letter;
+    braille_to_letter = {};
+    var letter_to_braille;
+    letter_to_braille = {};
 
     // Alphabet
     // a-j
@@ -47,131 +46,104 @@ function gen_unicode_maps() {
     i = 0;
     while (i < letters.length) {
         var b;
-        b = parse(unicode_hex_letters[i]);
-        unicode_to_glyph[b] = letters[i];
-        glyph_to_unicode[letters[i]] = b;
+        b = parse_unicode(unicode_hex_letters[i]);
+        braille_to_letter[b] = letters[i];
+        letter_to_braille[letters[i]] = b;
         i = i + 1;
     }
 
-    // Add numbers (0-9) to maps
+    // Add numbers (0-9) to letter_to_braille map ONLY
     var nums;
     nums = "1234567890"; // map to abcdefghij
     i = 0;
     while (i < nums.length) {
         var b;
-        b = parse(unicode_hex_letters[i]);
-        unicode_to_glyph[b] = nums[i];
-        glyph_to_unicode[nums[i]] = b;
+        b = parse_unicode(unicode_hex_letters[i]);
+        letter_to_braille[nums[i]] = b;
         i = i + 1;
     }
 
-    // Symbols
-    var unicode_hex_syms;
-    unicode_hex_syms = ['2800', '282e', '283c', '282b', '2829', '282f', '2804', '2837', '283e', '2821', '282c', '2820', '2824', '2828', '280c', '2834', '2802', '2806', '2812', '2832', '2822', '2816', '2836', '2826', '2814', '2831', '2830', '2823', '283f', '281c', '2839', '282a', '2833', '283b', '2818'];
+    // Punctuation
+    var unicode_hex_punc;
+    unicode_hex_punc = ['2800', '2804', '2824', '2834', '2802', '2806', '2812', '2832', '2816', '2836', '2836', '2826', '2818'];
 
-    // Add symbols to maps
-    var syms;
-    syms = " !#$%&\'()*+,-./0123456789:;<=>?[\\]^";
-    i = 0;
-    while (i < syms.length) {
-        var b;
-        b = parse(unicode_hex_syms[i]);
-        unicode_to_glyph[b] = syms[i];
-        glyph_to_unicode[syms[i]] = b;
-        i = i + 1;
-    }
-
-    return [unicode_to_glyph, glyph_to_unicode];
-}
-
-function gen_glyph_maps() {
-    /****************************************************
-    * Return [object mapping punctuation to ascii glyphs,
-    * object mapping ascii glyphs to punctuation, object mapping
-    * numbers to ascii glyphs, object mapping ascii glyphs to numbers] 
-    *
-    * Uses https://en.wikipedia.org/wiki/Braille_ASCII
-    ****************************************************/
-    // punctuation
-    var punc_to_glyph;
-    punc_to_glyph = {};
-    var glyph_to_punc;
-    glyph_to_punc = {};
-    // note: ( and ) map to same glyph
+    // Add punctuation to maps
     var punc;
-    punc = "\'-\",;:.!)(?$";
-    var punc_glyphs;
-    punc_glyphs = "\'-012346778^";
-    var i;
+    punc = " \'-\",;:.!)(?$";
     i = 0;
     while (i < punc.length) {
-        punc_to_glyph[punc[i]] = punc_glyphs[i];
-        glyph_to_punc[punc_glyphs[i]] = punc[i];
+        var b;
+        b = parse_unicode(unicode_hex_punc[i]);
+        braille_to_letter[b] = punc[i];
+        letter_to_braille[punc[i]] = b;
         i = i + 1;
     }
 
-    // numbers
-    var num_to_glyph;
-    num_to_glyph = {};
-    var glyph_to_num;
-    glyph_to_num = {};
-    var num_glyphs;
-    num_glyphs = "jabcdefghi";
+    // Contractions
+    var unicode_hex_contracts = ['282e', '282b', '2829', '282f', '2837', '283e', '2821', '282c', '280c', '2822', '2814', '2831', '2823', '283f', '281c', '2839'];
+
+    // Add contractions to maps
+    var contracts;
+    contracts = ['the', 'ed', 'sh', 'and', 'of', 'with', 'ch', 'ing', 'st', 'en', 'in', 'wh', 'gh', 'for', 'ar', 'th'];
     i = 0;
-    while (i < num_glyphs.length) {
-        num_to_glyph[i] = num_glyphs[i];
-        glyph_to_num[num_glyphs[i]] = i;
+    while (i < contracts.length) {
+        var b;
+        b = parse_unicode(unicode_hex_contracts[i]);
+        braille_to_letter[b] = contracts[i];
+        letter_to_braille[contracts[i]] = b;
         i = i + 1;
     }
 
-    return [punc_to_glyph, glyph_to_punc, num_to_glyph, glyph_to_num];
+    // Indicators
+    var unicode_hex_inds = ['283c', '2820', '2830'];
+
+    // Add indicators to maps
+    var inds;
+    inds = ['#', 'UC', 'LC']; // number, uppercase, lowercase
+    i = 0;
+    while (i < inds.length) {
+        var b;
+        b = parse_unicode(unicode_hex_inds[i]);
+        braille_to_letter[b] = inds[i];
+        letter_to_braille[inds[i]] = b;
+        i = i + 1;
+    }
+
+    return [braille_to_letter, letter_to_braille];
 }
 
 function get_char_type(c) {
     /****************************************************
-    * lowercase letter = 0, uppercase letter = 1, number = 2, other = 0
+    * lowercase = 'LC', uppercase = 'UC', number = '#', other = 'LC'
     ****************************************************/
-    if ((/[a-z]/).test(c)) {
-        return 0;
-    } else if ((/[A-Z]/).test(c)) {
-        return 1;
-    } else if ((/[0-9]/).test(c)) {
-        return 2;
-    } else {
-        return 0;
-    }
+    if ((/[A-Z]/).test(c)) {
+        return 'UC';
+    } 
+    if ((/[0-9]/).test(c)) {
+        return '#';
+    } 
+    return 'LC';
 }
 
-function text_to_glyphs(text) {
+function text_to_braille(text) {
     /****************************************************
-    * Convert text to ascii glyphs
+    * Convert text to braille 
     ****************************************************/
-    // 0 = lowercase letter, 1 = uppercase letter, 2 = number
-    var ind_glyphs;
-    ind_glyphs = [';', ',', '#'];
+    // 'LC' = lowercase, 'UC' = uppercase, '#' = number
     var prev_char_type;
-    prev_char_type = 0;
+    prev_char_type = 'LC';
 
     // maps
-    var glyph_maps;
-    glyph_maps = gen_glyph_maps();
-    var punc_to_glyph;
-    punc_to_glyph = glyph_maps[0];
-    num_to_glyph = glyph_maps[2];
-
-    // regex
-    var punc_regex;
-    punc_regex = (/'|-|"|,|;|:|.|!|\(|\)|\?|$/);
-    var alpha_regex;
-    alpha_regex = (/[a-zA-Z]/);
-    var num_regex;
-    num_regex = (/[0-9]/);
+    var maps;
+    maps = gen_maps();
+    var letter_to_braille;
+    letter_to_braille = maps[1];
 
     // iterate over words in text
     var words;
     words = text.split(" ");
-    var glyphs;
-    glyphs = "";
+    var braille;
+    braille = "";
     var i;
     i = 0;
     while (i < words.length) {
@@ -188,90 +160,125 @@ function text_to_glyphs(text) {
         var j;
         j = 0;
         while (j < word_parts.length) {
-            var p;
-            p = word_parts[j];
+            var wp;
+            wp = word_parts[j];
 
-            // if length of word part is > 1, it is a contraction
-            if (p.length > 1) {
-                // TODO: parse contractions
-            } else { // else it is a letter / number / punctuation mark
-                var char_type;
-                char_type = get_char_type(p);
+            var char_type;
+            char_type = get_char_type(wp);
 
-                // add indicator if first letter is upper case or if
-                // going from letter to number / number to letter
-                if ((j == 0) && (char_type == 1)) { 
-                    glyphs = glyphs + ind_glyphs[char_type];
-                } else if ( (char_type < 2) ^ (prev_char_type < 2) ) {
-                    glyphs = glyphs + ind_glyphs[char_type];
-                }
-
-                if (alpha_regex.test(p)) {
-                    glyphs = glyphs + p.toLowerCase();
-                } else if (num_regex.test(p)) {
-                    glyphs = glyphs + num_to_glyph[p];
-                } else if (punc_regex.test(p)) {
-                    glyphs = glyphs + punc_to_glyph[p];
-                } else {
-                    console.log("Error: invalid character " + c + ": no mapping found.");
-                }
+            // add indicator if first letter is upper case or if
+            // going from letter to number / number to letter
+            if ((j == 0) && (char_type == 'UC')) { 
+                braille = braille + letter_to_braille[char_type];
+            } else if ( (char_type == '#') ^ (prev_char_type == '#') ) {
+                braille = braille + letter_to_braille[char_type];
+            }
+            
+            // convert letter(s) to braille
+            // letter_to_braille object keys only have lowercase letters
+            if (char_type == 'UC') {
+                wp = wp.toLowerCase();
+            }
+            if (wp in letter_to_braille) {
+                braille = braille + letter_to_braille[wp];
+            } else {
+                console.log("Error: invalid character " + wp + ": no mapping found.");
             }
 
             j = j + 1;
         }
         
         // add space after word
-        glyphs = glyphs + " ";
-        // reset previous char type at end of word
-        prev_char_type = 0;
+        braille = braille + letter_to_braille[' '];
+        // reset previous character type at end of word
+        prev_char_type = 'LC';
         i = i + 1;
     }
 
-    return glyphs;
+    return braille;
 }
 
-// TODO: Create glyphs_to_text function
-
-function glyphs_to_braille(glyphs) {
+function braille_to_text(braille) {
     /****************************************************
-    * Convert ascii glyphs to braille unicode
+    * Convert braille to text
     ****************************************************/
-    // get map
-    var unicode_maps;
-    unicode_maps = gen_unicode_maps();
-    var glyph_to_unicode;
-    glyph_to_unicode = unicode_maps[1];
+    // maps
+    var maps;
+    maps = gen_maps();
+    var braille_to_letter;
+    braille_to_letter = maps[0];
+    var letter_to_braille;
+    letter_to_braille = maps[1];
+    var letters_to_nums;
+    letters_to_nums = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9, 'j':0};
 
-    // convert glyphs to braille
-    var braille;
-    braille = "";
+    // character types
+    var char_types;
+    char_types = new Set(['LC', 'UC', '#']);
+    var current_char_type;
+    current_char_type = 'LC';
+
+    // iterate over braille words
+    words = braille.split(letter_to_braille[' ']);
+    var text;
+    text = "";
     var i;
     i = 0;
-    while (i < glyphs.length) {
-        braille = braille + glyph_to_unicode[glyphs[i]];
+    while (i < words.length) {
+        // get word
+        var word;
+        word = words[i];
+         
+        // parse word
+        var j;
+        j = 0;
+        while (j < word.length) {
+            var b; // braille character
+            b = word[j];
+
+            var wp; // word part
+            if (b in braille_to_letter) {
+                wp = braille_to_letter[b];
+                if (char_types.has(wp)) {
+                    current_char_type = wp;
+                } else {
+                    if (current_char_type == '#') {
+                        text = text + letters_to_nums[wp];
+                    } else if (current_char_type == 'UC' ) {
+                        text = text + wp.substring(0,1).toUpperCase() + wp.substring(1);
+                        current_char_type = 'LC';
+                    } else {
+                        text = text + wp;
+                    }
+                }
+            } else {
+                console.log("Error: invalid braille symbol " + b + ": no mapping found.");
+            }
+
+            j = j + 1;
+        }
+        
+        // add space after word
+        text = text + " ";
+        // reset character type at end of word
+        current_char_type = 'LC';
         i = i + 1;
     }
 
-    return braille;
+    return text;
 }
-
-// TODO: Create braille_to_glyphs function
-
-function text_to_braille(text) {
-    var glyphs;
-    glyphs = text_to_glyphs(text);
-    var braille;
-    braille = glyphs_to_braille(glyphs);
-    return braille;
-}
-
-// TODO: Create braille to text function
 
 function test() {
-    var text;
-    text = "This is a test.";
-    console.log(text);
-    console.log(text_to_braille(text));
+    var text1;
+    text1 = "This is a test.";
+    var braille1;
+    braille1 = text_to_braille(text1);
+    console.log(text1);
+    console.log(braille1);
+
+    var text2;
+    text2 = braille_to_text(braille1);
+    console.log(text2);
 }
 
 test();
